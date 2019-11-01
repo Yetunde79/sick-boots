003e10af0b7fc6d97ceed2e11ebeec7913fee6d1
Yetunde Sola-Adebayo <yetundesola-adebayo@Yetundes-Air.attlocal.net>
2019-11-01 13:38:04 -0500

    getting graphql yoga server running

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const mutations = {};
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
 
-module.exports = mutations;
+    //create a dog
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    return newDog;
+  }
+};
+
+module.exports = Mutations;
