5b2e8a804ce143d90d9354b4cc6a161a45e1c7f1
Yetunde Sola-Adebayo <yetundesola-adebayo@Yetundes-Air.attlocal.net>
2019-12-10 12:30:12 -0600

    pagination and cache invalidation

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -5,10 +5,11 @@ import styled from "styled-components";
 
 import Item from "./Item";
 import Pagination from "./Pagination";
+import { perPage } from "../config";
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -34,7 +35,13 @@ class Items extends Component {
     return (
       <Center>
         <Pagination page={this.props.page}></Pagination>
-        <Query query={ALL_ITEMS_QUERY}>
+        <Query
+          query={ALL_ITEMS_QUERY}
+          fetchPolicy="network-only"
+          variables={{
+            skip: this.props.page * perPage - perPage
+          }}
+        >
           {({ data, error, loading }) => {
             if (loading) return <p>loading...</p>;
             if (error) return <p>Error: {error.message}</p>;
