870d32360e25552838d7b2c4622d9783695a24b5
Yetunde Sola-Adebayo <yetundesola-adebayo@Yetundes-Air.attlocal.net>
2020-01-07 13:01:02 -0600

    added user signup ui

---
+++ frontend/pages/signup.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import SignUp from "../components/Signup";
+import styled from "styled-components";
+
+const Columns = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+`;
+
+const SignUpPage = props => (
+  <Columns>
+    <SignUp />
+    <SignUp />
+    <SignUp />
+  </Columns>
+);
+
+export default SignUpPage;
